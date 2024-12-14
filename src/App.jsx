c1c26bc071c8d10896b399f34f350d07e88c8dd4
zei114 <cactus1609@gmail.com>
2024-12-14 23:52:18 +0700

    Add sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-  return (  
-	  <div>
-		<Login />
-	  </div>
-  );
+	return (  
+		<>
+			<div className="mb-6">
+				<Login />
+			</div>
+			
+			<div>
+				<SignUp />
+			</div>
+		</>
+	);
 }
 
 export default App
