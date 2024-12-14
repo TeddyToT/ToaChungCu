debfdbf58982d94aac3a049241ef8f47435deb82
zei114 <cactus1609@gmail.com>
2024-12-14 20:52:29 +0700

    Add Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -28,6 +31,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+	  
+	  <div>
+		<Login />
+	  </div>
     </>
   )
 }
